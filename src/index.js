8d25081260730a16d33d380bf6d19c02f9edea08
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2015-12-31 18:33:39 -0500

    Added registry of basic citation renderer.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+import env from './env'
+import * as registry from './registry'
 import article from './transforms/article'
 import diagrams from './transforms/diagrams'
 import endnotes from './transforms/endnotes'
 import citations from './transforms/citations'
 import idempotentiate from './transforms/idempotentiate'
+import * as basic from './citation-renderers/basic'
 
-export default function (markup) {
+function transpiler (markup) {
   let content = article(markup)
   if (!content) return markup
   return (
@@ -21,3 +24,15 @@ export default function (markup) {
     )
   )
 }
+
+transpiler.style = function (name, inText, full) {
+  registry.set('styles', name, {
+    inText: inText,
+    full: full
+  })
+}
+
+// Register core citation styles
+transpiler.style('basic', basic.inText, basic.full)
+
+export default transpiler
