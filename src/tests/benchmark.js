b6b61516aa337bbeefebed848b02db7fc3e1fb4f
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2016-01-02 12:17:18 -0500

    Added labcoat() invocation before test

--- src/tests/benchmark.js
+++ src/tests/benchmark.js
@@ -4,6 +4,7 @@ import labcoat from '../../build'
 const HTML = fs.readFileSync(__dirname + '/../../benchmark.html', 'utf8')
 
 export default function () {
+  labcoat('warm up the engine')
   let perf = process.hrtime()
   let output = labcoat(HTML)
   let duration = process.hrtime(perf)
@@ -13,7 +14,7 @@ export default function () {
   Benchmark:
     Input: ${HTML.length} characters
     Output: ${output.length} characters
-    Duration: ${(duration[1] * 1e-6)}ms
+    Duration: ${Math.round(duration[1] * 1e-3)}μs (${(duration[1] * 1e-6)}ms)
 
 `
   )
