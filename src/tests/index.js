6c209768b5c17fdde736f1e62f1d08659bc99ced
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2016-01-11 14:17:05 -0500

    Added mla tests

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,7 +1,7 @@
 import bodyTests from './body'
 import endnoteTests from './endnotes'
 import citationTests from './citations'
-import apaTests from '../styles/tests'
+import styleTests from '../styles/tests'
 import diagramTests from './diagrams'
 import benchmark from './benchmark'
 
@@ -9,7 +9,7 @@ process.stdout.write('\n')
 bodyTests()
 endnoteTests()
 citationTests()
-apaTests()
+styleTests()
 diagramTests()
 process.stdout.write('  ✔ All tests passing.')
 benchmark()
