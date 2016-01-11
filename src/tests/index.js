9033e8afcec6b223bedee3a3d502431911e75003
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2016-01-11 13:02:17 -0500

    Added APA tests

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,13 +1,15 @@
-import bodyTests from '../tests/body'
-import endnoteTests from '../tests/endnotes'
-import citationTests from '../tests/citations'
-import diagramTests from '../tests/diagrams'
-import benchmark from '../tests/benchmark'
+import bodyTests from './body'
+import endnoteTests from './endnotes'
+import citationTests from './citations'
+import apaTests from '../styles/tests'
+import diagramTests from './diagrams'
+import benchmark from './benchmark'
 
 process.stdout.write('\n')
 bodyTests()
 endnoteTests()
 citationTests()
+apaTests()
 diagramTests()
 process.stdout.write('  ✔ All tests passing.')
 benchmark()
