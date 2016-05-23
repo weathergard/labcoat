024e6c764e2194a872866ec7a20054b230761155
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2016-05-23 15:16:13 -0400

    Added inline styles and classes

--- src/tests/endnotes.js
+++ src/tests/endnotes.js
@@ -19,13 +19,13 @@ const HTML = [
   ],
   [
     '<endnotes />',
-    '<section class="endnotes"><ol class="endnotes-list"></ol></section>'
+    '<section class="endnotes"><ol class="endnotes-list" style=""></ol></section>'
   ],
   [
     '<endnote>Contents</endnote><endnotes/>',
     '<sup><a id="intext-note-1" href="#endnote-1">1</a></sup>'+
     '<section class="endnotes">'+
-      '<ol class="endnotes-list">'+
+      '<ol class="endnotes-list" style="">'+
         '<li><a id="endnote-1" href="#intext-note-1">Contents</a></li>'+
       '</ol>'+
     '</section>'
@@ -34,7 +34,7 @@ const HTML = [
     '<endnote>Contents</endnote><endnotes roman />',
     '<sup><a id="intext-note-i" href="#endnote-i">i</a></sup>'+
     '<section class="endnotes">'+
-      '<ol class="endnotes-list">'+
+      '<ol class="endnotes-list roman-numbering" style="list-style:lower-roman;">'+
         '<li><a id="endnote-i" href="#intext-note-i">Contents</a></li>'+
       '</ol>'+
     '</section>'
@@ -43,7 +43,7 @@ const HTML = [
     '<endnote>Contents</endnote><endnotes latin />',
     '<sup><a id="intext-note-a" href="#endnote-a">a</a></sup>'+
     '<section class="endnotes">'+
-      '<ol class="endnotes-list">'+
+      '<ol class="endnotes-list latin-numbering" style="list-style:lower-latin;">'+
         '<li><a id="endnote-a" href="#intext-note-a">Contents</a></li>'+
       '</ol>'+
     '</section>'
